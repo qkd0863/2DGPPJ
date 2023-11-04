8fb64a0d6c90ba5e75375a0caa62e9f3aec6e73b
qkd0863 <qkd7183@naver.com>
2023-11-05 01:29:14 +0900

    이미지 이동 및 게임월드

--- car.py
+++ car.py
@@ -1,14 +1,120 @@
 from pico2d import load_image
+from sdl2 import SDL_KEYDOWN, SDLK_LEFT, SDLK_RIGHT, SDL_KEYUP
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+class Idle:
+    @staticmethod
+    def enter(car, e):
+        car.dir = 0
+        print('Idle Enter')
+
+    @staticmethod
+    def exit(car, e):
+        print('Idle Exit')
+
+    @staticmethod
+    def do(car):
+        car.frame = (car.frame + 1) % 2
+        print('Idle Do')
+
+    @staticmethod
+    def draw(car):
+        if car.frame == 0:
+            car.image.clip_draw(0, 0, 40, 26, car.x, car.y, 100, 50)
+        if car.frame == 1:
+            car.image.clip_draw(40, 0, 56, 26, car.x, car.y, 100, 50)
+        pass
+
+
+class Move:
+    @staticmethod
+    def enter(car, e):
+        if right_down(e):
+            car.dir = 1
+        elif left_down(e):
+            car.dir = -1
+        print('Move Enter')
+
+    @staticmethod
+    def exit(car, e):
+        print('Move Exit')
+
+    @staticmethod
+    def do(car):
+        car.frame = (car.frame + 1) % 2
+
+        if car.x + car.dir * 5 > 100 and car.x + car.dir * 5 < 700:
+            car.x += car.dir * 5
+        print('Move Do')
+
+    @staticmethod
+    def draw(car):
+        if car.frame == 0:
+            car.image.clip_draw(0, 0, 40, 26, car.x, car.y, 100, 50)
+        if car.frame == 1:
+            car.image.clip_draw(40, 0, 56, 26, car.x, car.y, 100, 50)
+        pass
+
+
+class StateMachine:
+    def __init__(self, car):
+        self.car = car
+        self.cur_state = Idle
+        self.transitions = {
+            Idle: {left_down: Move, right_down: Move, left_up: Move, right_down: Move},
+            Move: {left_up: Idle, right_up: Idle}
+        }
+
+    def start(self):
+        self.cur_state.enter(self.car, ('START', 0))
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.car, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.car, e)
+                return True
+
+        return False
+
+    def update(self):
+        self.cur_state.do(self.car)
+
+    def draw(self):
+        self.cur_state.draw(self.car)
 
 
 class Car:
-    def __init__(self, x, y):
+    def __init__(self):
+        self.x, self.y = 300, 300
+        self.frame = 0
         self.image = load_image('12.png')
-        self.x = x
-        self.y = y
-
-    def draw(self):
-        self.image.clip_draw(0, 0, 40, 50, 200, 100)
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
 
     def update(self):
+        self.state_machine.update()
+
+    def handle_events(self, event):
+        self.state_machine.handle_event(('INPUT', event))
         pass
+
+    def draw(self):
+        self.state_machine.draw()
--- race.py
+++ race.py
@@ -4,35 +4,21 @@ import game_world
 from car import Car
 
 
-# Game object class here
-
-
 def handle_events():
     global running
-
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-
-
-
-def create_world():
-    global running
-    global grass
-    global team
-    global boy
-
-    running = True
-
-    car = Car(100,100)
-    game_world.add_object(car,1)
+        else:
+            car.handle_events(event)
 
 
 def update_world():
     game_world.update()
+    pass
 
 
 def render_world():
@@ -41,13 +27,32 @@ def render_world():
     update_canvas()
 
 
+
+def reset_world():
+    global running
+    global car
+    running = True
+
+    car = Car()
+
+
+def create_world():
+    global running
+    global car
+
+    running = True
+
+    car = Car()
+    game_world.add_object(car, 1)
+
+
 open_canvas()
 create_world()
-# game loop
+
 while running:
     handle_events()
     update_world()
     render_world()
     delay(0.01)
-# finalization code
+
 close_canvas()
