c8237c65a208683ca8d3cf3023818cf7af01881c
qkd0863 <qkd7183@naver.com>
2023-11-14 23:24:31 +0900

    발표 준비

--- title_mode.py
+++ title_mode.py
@@ -7,26 +7,32 @@ import play_mode
 
 
 def init():
-     global image
-     image = load_image('title.png')
+    global image
+    image = load_image('title.png')
+
+
 def finish():
-     global image
-     del image
+    global image
+    del image
 
 
 def update():
     handle_events()
     pass
+
+
 def handle_events():
-     events = get_events()
-     for event in events:
-         if event.type == SDL_QUIT:
-             game_framework.quit()
-         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-             game_framework.quit()
-         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-             game_framework.change_mode(play_mode)
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_mode(play_mode)
+
+
 def draw():
-     clear_canvas()
-     image.draw(400,300)
-     update_canvas()
+    clear_canvas()
+    image.draw(400, 300, 800, 600)
+    update_canvas()
